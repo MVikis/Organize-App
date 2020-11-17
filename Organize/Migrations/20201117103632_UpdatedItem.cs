64242d0b2be4aa2ceafb3c795086a6d63ec47dc5
Martin Viksten <vikstenmartin@gmail.com>
2020-11-17 03:43:05 -0800

    Changed around in components

---
+++ Organize/Migrations/20201117103632_UpdatedItem.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Organize.Migrations
+{
+    public partial class UpdatedItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Items",
+                table: "Containers");
+
+            migrationBuilder.CreateTable(
+                name: "Item",
+                columns: table => new
+                {
+                    ItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ItemName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContainerId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Item", x => x.ItemId);
+                    table.ForeignKey(
+                        name: "FK_Item_Containers_ContainerId",
+                        column: x => x.ContainerId,
+                        principalTable: "Containers",
+                        principalColumn: "ContainerId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Item_ContainerId",
+                table: "Item",
+                column: "ContainerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Item");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Items",
+                table: "Containers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
