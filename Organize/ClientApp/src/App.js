321e97445a854022f27c0087309c4d9858c68c19
Martin Viksten <vikstenmartin@gmail.com>
2020-11-17 02:23:40 -0800

    Added Box, UserPage comps

--- Organize/ClientApp/src/App.js
+++ Organize/ClientApp/src/App.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 
+import { Counter } from './components/Counter';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faBoxOpen } from '@fortawesome/free-solid-svg-icons'
 import './custom.css'
+import './style.css'
+
+import UserPage from './components/UserPage';
+library.add(fab, faCheckSquare, faBoxOpen)
 
 export default class App extends Component {
   static displayName = App.name;
@@ -14,8 +20,8 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
+        <Route path='/user' component={UserPage} />
+       
       </Layout>
     );
   }
---
+++ Organize/ClientApp/src/components/UserPage.js
@@ -0,0 +1,20 @@
+﻿import React, { useState } from 'react'
+import Box from './Box'
+
+export default function UserPage(props) {
+
+const[Containers, setContainers] =useState([])
+    
+  const populateContainerData=async()=> {
+    const response = await fetch('api/Containers');
+    const data = await response.json();
+    setContainers(data)
+  }
+
+    return (
+        <div className="row">
+                <Box />
+            
+        </div>
+        )
+}
